c826bf3a803264c61e473fde1f0ee3051a03d23c
Nick Ellsworth <nellsworth@spotify.com>
2019-10-22 15:42:11 -0400

    Cubes

--- scripts/index.js
+++ scripts/index.js
@@ -8,9 +8,8 @@ var OrbitControls = require('three-orbit-controls')(THREE)
 import Physijs from 'physijs-webpack';
 import PhysijsWorker from 'physijs-webpack/physijs_worker';
 
-let state = {
-  
-}
+
+let boxes = []
 
 
 function onMIDIMessage( event ) {
@@ -33,29 +32,26 @@ listen()
 var friction = 0.8; // high friction
 var restitution = 0.2; // low restitution
 
-var material = Physijs.createMaterial(
-    new THREE.MeshStandardMaterial({ color: 0x888888, emissive: 0xFFFFFF, }),
-    friction,
-    restitution
-);
 
 
 let initScene, render, renderer, scene, camera, box, floor, composer;
 
 function onNoteOn(note, velocity){
-  const boxVectorY = 500 + ((velocity / 128 ) * 1500)
-  box.applyCentralImpulse(new THREE.Vector3(0, boxVectorY, 0))
+  const boxVectorY = 1000 + ((velocity / 128 ) * 2000)
+  boxes[note].model.applyCentralImpulse(new THREE.Vector3(0, boxVectorY, 0))
+  boxes[note].model.material.emissive = boxes[note].color
 }
 
-function onNoteOff() {
+function onNoteOff(note) {
+  boxes[note].model.material.emissive = new THREE.Color(0, 0, 0)
   // box.setLinearVelocity(new THREE.Vector3(0, 0, 0))
 }
 
 initScene = function init() {
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer = new THREE.WebGLRenderer({ antialias: false });
   renderer.setSize( window.innerWidth, window.innerHeight );
   
-  renderer.shadowMap.enabled = true;
+  // renderer.shadowMap.enabled = true;
   scene = new Physijs.Scene;
   
   camera = new THREE.PerspectiveCamera(
@@ -69,47 +65,65 @@ initScene = function init() {
   
   document.getElementById( 'viewport' ).appendChild( renderer.domElement );
   var renderScene = new RenderPass( scene, camera );
-  var bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+  var bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 0.8, 0.4, 0.0 );
   
   composer = new EffectComposer( renderer );
   composer.addPass( renderScene );
   composer.addPass( bloomPass );
   
-  scene.setGravity(new THREE.Vector3( 0, -100, 0 ));
-
+  scene.setGravity(new THREE.Vector3( 0, -300, 0 ));
   
   const controls = new OrbitControls( camera, renderer.domElement );
 
   scene.add( camera );
   
-  // Box
-  box = new Physijs.BoxMesh(
-    new THREE.CubeGeometry( 5, 5, 5 ),
-    material,
-    50,
-  );
-  box.castShadow = true
-
   floor = new Physijs.BoxMesh(
-    new THREE.BoxGeometry(50, 2, 50),
-    new THREE.MeshStandardMaterial({ color: 0x444444 }),
+    new THREE.BoxGeometry(100, 1, 100),
+    new THREE.MeshStandardMaterial({ color: 0x666666, colorWrite: false }),
     0
   )
   floor.position.set(0, -6, 0)
-
-  floor.receiveShadow = true
+  
   
   scene.add( floor );
-  scene.add( box );
   // White directional light at half intensity shining from the top.
-  var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-  pointLight.position.set(10, 20, 10)
-  pointLight.castShadow = true
-
+  // var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+  // pointLight.position.set(10, 20, 10)
+  // pointLight.castShadow = true
+  
   var ambientLight = new THREE.AmbientLight( 0xffffff, 0.8 );
+  
+  scene.add( ambientLight );
+  
 
-  scene.add( pointLight );
-  // scene.add( ambientLight );
+  // The fun stuff
+  
+
+  
+  let start = 43
+  for (let i = start; i < start + 12; i++ ) {
+    const colors = [new THREE.Color(1, 0, 0), new THREE.Color(0, 1, 0), new THREE.Color(0, 0, 1)]
+    
+    let color = colors[i%3]
+    let box = {
+      color,
+      model: new Physijs.BoxMesh(
+        new THREE.CubeGeometry( 5, 5, 5 ),
+        Physijs.createMaterial(
+          new THREE.MeshStandardMaterial({ color: new THREE.Color(0.5, 0.5, 0.5), emissive: new THREE.Color(0, 0, 0), }),
+          friction,
+          restitution
+        ),
+        50,
+      ),
+      state: {
+
+      }
+    }
+    boxes[i] = box
+    box.model.position.set(((i - start - 5) * 6), 0, 0)
+    scene.add( box.model );
+  }
   
   requestAnimationFrame( render );
 };
@@ -121,3 +135,13 @@ render = function() {
 };
 
 initScene()
+
+
+window.onresize = function () {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize( width, height );
+  composer.setSize( width, height );
+};
